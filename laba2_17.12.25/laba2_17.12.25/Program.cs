efa09f19b7ecae678a3e25d6b165f36b2a792c9a
vebikt <vebikt@gmail.com>
2025-12-17 20:31:05 +0300

    C#

---
+++ laba2_17.12.25/laba2_17.12.25/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace laba2_17._12._25
+{
+    class Message
+    {
+
+        public virtual void ShowMessage()
+        {
+
+        }
+    }
+    class Hello : Message
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+        public override void ShowMessage()
+        {
+            MessageBox(IntPtr.Zero, "Привет, мир!", "Ура-ура!", 0 | 0x00000040);
+        }
+    }
+    class Bye : Message
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+        public override void ShowMessage()
+        {
+            MessageBox(IntPtr.Zero, "Пока!", "Ура-ура!", 0 | 0x00000040);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {     
+            Message message = new Hello();
+            Message message1 = new Bye();
+
+            message.ShowMessage();
+            message1.ShowMessage();
+
+        }
+    }
+}
