9a641254fdcde16a51d4cb57c38b1b3e2b5294f8
vebikt <vebikt@gmail.com>
2026-01-12 20:27:46 +0300

    C#
    Месенджер

---
+++ messeg/messeg/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace messeg
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            const int port = 5000;
+
+            TcpListener server = new TcpListener(IPAddress.Any, port);
+
+            server.Start();
+            Console.WriteLine("Сервер запущен");
+
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Клиент тут");
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string mess = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine("Сообщение: "+ mess);
+
+            string response = "Сообщение получино сервером";
+            byte[] responseData = Encoding.UTF8.GetBytes(response);
+
+            stream.Write(responseData, 0, responseData.Length);
+
+            client.Close();
+            server.Stop();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ messegClient/messegClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+namespace messegClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("192.168.0.3", 5000);
+
+            NetworkStream stream = client.GetStream();
+            Console.WriteLine("Введите сообщение: ");
+            string mess = Console.ReadLine();
+
+            byte[] data = Encoding.UTF8.GetBytes(mess);
+            stream.Write(data, 0, data.Length);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Ответ сервера: "+ response);
+
+            client.Close();
+            Console.ReadKey();
+        }
+    }
+}
