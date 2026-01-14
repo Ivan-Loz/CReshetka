a96d47cc8834070b3d64d09fa35e0b051eae3de0
vebikt <vebikt@gmail.com>
2026-01-14 20:28:38 +0300

    С#
    Новый чат

---
+++ ClientTop/ClientTop/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ClientTop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("192.168.0.3", 5000);
+
+            NetworkStream stream = client.GetStream();
+
+            Thread receiveTheread = new Thread(() =>
+            {
+                byte[] buffer = new byte[1024];
+
+                while (true)
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string mess = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                    Console.WriteLine(mess);
+                }
+            });
+
+            receiveTheread.Start();
+
+            Console.WriteLine("Введитк ник: ");
+            string name = Console.ReadLine();
+
+            while (true)
+            {
+                string mess = Console.ReadLine();
+                string fullMess = name + ": " + mess;
+
+                byte[] data = Encoding.UTF8.GetBytes(fullMess);
+
+                stream.Write(data, 0, data.Length);
+
+            }
+        }
+    }
+}
---
+++ chatServerTop/chatServerTop/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace chatServerTop
+{
+    class Program
+    {
+        static List<TcpClient> clients = new List<TcpClient>();
+        static void Main(string[] args)
+        {
+            const int port = 5000;
+
+            TcpListener server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            Console.WriteLine("Сервер запущен");
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                clients.Add(client);
+                Console.WriteLine("Клиент тут");
+
+                Thread clientThread = new Thread(HandleClient);
+                clientThread.Start(client);
+
+            }
+
+            
+        }
+        static void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            while (true)
+            {
+                int bytesRead;
+
+                try
+                {
+                    bytesRead = stream.Read(buffer, 0, buffer.Length);
+                }
+                catch
+                {
+                    break;
+                }
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+                string mess = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                BroadcastMessage(mess, client);
+
+            }
+            clients.Remove(client);
+            client.Close();
+
+        }
+        static void BroadcastMessage(string mess,TcpClient sender)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(mess);
+
+            foreach (TcpClient client in clients)
+            {
+                if (client != sender)
+                {
+                    NetworkStream stream = client.GetStream();
+                    stream.Write(data, 0, data.Length);
+
+                }
+            }
+            Console.WriteLine(mess);
+        }
+    }
+}
