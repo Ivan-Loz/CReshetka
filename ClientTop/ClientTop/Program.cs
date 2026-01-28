ecb1db1b97776a45f308a95a22dec30fa14e9083
vebikt <vebikt@gmail.com>
2026-01-28 18:59:59 +0300

    Client
    Клиент для нашего сервера

--- ClientTop/ClientTop/Program.cs
+++ ClientTop/ClientTop/Program.cs
@@ -1,50 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 
-namespace ClientTop
+namespace ChatClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             TcpClient client = new TcpClient();
-            client.Connect("192.168.0.3", 5000);
+            client.Connect("127.0.0.1", 5000);
 
             NetworkStream stream = client.GetStream();
 
-            Thread receiveTheread = new Thread(() =>
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            byte[] nameData = Encoding.UTF8.GetBytes(name);
+            stream.Write(nameData, 0, nameData.Length);
+
+            Thread receiveThread = new Thread(() =>
             {
                 byte[] buffer = new byte[1024];
 
                 while (true)
                 {
-                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                    string mess = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    try
+                    {
+                        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                        if (bytesRead == 0)
+                            break;
 
-                    Console.WriteLine(mess);
+                        string message =
+                            Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        Console.Write(message);
+                    }
+                    catch
+                    {
+                        break;
+                    }
                 }
             });
 
-            receiveTheread.Start();
-
-            Console.WriteLine("Введитк ник: ");
-            string name = Console.ReadLine();
+            receiveThread.Start();
 
             while (true)
             {
-                string mess = Console.ReadLine();
-                string fullMess = name + ": " + mess;
-
-                byte[] data = Encoding.UTF8.GetBytes(fullMess);
-
+                string message = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(message);
                 stream.Write(data, 0, data.Length);
-
             }
         }
     }
